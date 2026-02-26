5be8385b9687b92fbeb89cb10c1b844ea744edc9
rimi28-cpu <rimiikarashi@gmail.com>
2026-02-27 00:55:29 +0530

    Update interactions.py

--- api/interactions.py
+++ api/interactions.py
@@ -1,11 +1,11 @@
 async def app(scope, receive, send):
-    assert scope['type'] == 'http'
-    await send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [(b'content-type', b'text/plain')],
-    })
-    await send({
-        'type': 'http.response.body',
-        'body': b'Hello ASGI',
-    })
+    if scope['type'] == 'http':
+        await send({
+            'type': 'http.response.start',
+            'status': 200,
+            'headers': [[b'content-type', b'text/plain']]
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': b'ASGI works!'
+        })
