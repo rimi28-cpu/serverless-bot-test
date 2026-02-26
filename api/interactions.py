b70bb773259b6ae66ae2ab7fcb6b6987b952289b
rimi28-cpu <rimiikarashi@gmail.com>
2026-02-27 00:57:51 +0530

    Update interactions.py

--- api/interactions.py
+++ api/interactions.py
@@ -1,11 +1,20 @@
-async def app(scope, receive, send):
-    if scope['type'] == 'http':
-        await send({
-            'type': 'http.response.start',
-            'status': 200,
-            'headers': [[b'content-type', b'text/plain']]
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': b'ASGI works!'
-        })
+import os
+import discohook
+
+# Initialize the discohook client (ASGI app)
+app = discohook.Client(
+    application_id=os.environ["APPLICATION_ID"],
+    public_key=os.environ["PUBLIC_KEY"],
+    token=os.environ["DISCORD_TOKEN"],
+    default_help_command=True,
+)
+
+# Add a simple ping command
+@app.load
+@discohook.command.slash()
+async def ping(i: discohook.Interaction):
+    """Replies with Pong!"""
+    await i.response.send("Pong!")
+
+# Export the ASGI app as the Vercel handler
+handler = app
