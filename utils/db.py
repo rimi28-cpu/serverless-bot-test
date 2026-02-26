d835327de174484a9825f6bc80f76e1190a19005
rimi28-cpu <rimiikarashi@gmail.com>
2026-02-27 01:21:29 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,10 +1,11 @@
 import os
-import asyncpg
 
 _pool = None
 
 async def get_db_pool():
     global _pool
     if _pool is None:
+        import asyncpg  # Import here instead of top-level
         _pool = await asyncpg.create_pool(os.environ["NEON_DATABASE_URL"])
     return _pool
+    
